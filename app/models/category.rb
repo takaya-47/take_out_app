7d2271e9929daeb9e284562f264169aebbf6da0d
takaya-47 <local.s.s.47@gmail.com>
2021-03-03 10:39:59 +0900

    アクティブハッシュのデータ中身を記述

---
+++ app/models/category.rb
@@ -0,0 +1,14 @@
+class Category < ActiveHash::Base
+  self.data = [
+    {id: 0, name: "和食"},
+    {id: 1, name: "イタリアン"},
+    {id: 2, name: "フレンチ"},
+    {id: 3, name: "中華"},
+    {id: 4, name: "アジア・エスニック"},
+    {id: 5, name: "ファーストフード"},
+    {id: 6, name: "カフェ・喫茶"},
+    {id: 7, name: "スイーツ"},
+    {id: 8, name: "パン屋"},
+    {id: 9, name: "その他"}
+  ]
+end
