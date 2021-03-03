c83dc2234b6263beea92869d5ec6eea6e2cf9e18
takaya-47 <local.s.s.47@gmail.com>
2021-03-03 19:59:11 +0900

    リセットCSS導入、ヘッダーのスタイル調整まで

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
-  def index
+  def home
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  root 'static_pages#home'
+  get 'static_pages/contact', to: "static_pages#contact"
   devise_for :users
 end
