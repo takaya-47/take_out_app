a8da7a9bbd3422e7323270f81ceecbfb582f4d5d
takaya-47 <local.s.s.47@gmail.com>
2021-03-03 12:37:05 +0900

    RuboCopの実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:shop_name, :category_id, :prefecture_id, :address, :business_hours, :holiday, :phone_number])
-    end
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:shop_name, :category_id, :prefecture_id, :address, :business_hours, :holiday,
+                                             :phone_number])
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,16 @@
 class Category < ActiveHash::Base
   self.data = [
-    {id: 0, name: '--'},
-    {id: 1, name: "和食"},
-    {id: 2, name: "イタリアン"},
-    {id: 3, name: "フレンチ"},
-    {id: 4, name: "中華"},
-    {id: 5, name: "アジア・エスニック"},
-    {id: 6, name: "ファーストフード"},
-    {id: 7, name: "カフェ・喫茶"},
-    {id: 8, name: "スイーツ"},
-    {id: 9, name: "パン屋"},
-    {id: 10, name: "その他"}
+    { id: 0, name: '--' },
+    { id: 1, name: '和食' },
+    { id: 2, name: 'イタリアン' },
+    { id: 3, name: 'フレンチ' },
+    { id: 4, name: '中華' },
+    { id: 5, name: 'アジア・エスニック' },
+    { id: 6, name: 'ファーストフード' },
+    { id: 7, name: 'カフェ・喫茶' },
+    { id: 8, name: 'スイーツ' },
+    { id: 9, name: 'パン屋' },
+    { id: 10, name: 'その他' }
   ]
 
   include ActiveHash::Associations
