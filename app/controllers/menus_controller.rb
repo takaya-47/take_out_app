d18b45d4e69dc42f464a8f13af0b6bb985934062
takaya-47 <local.s.s.47@gmail.com>
2021-03-06 17:08:59 +0900

    newとupdateアクション、フラッシュメッセージ

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -4,5 +4,17 @@ class MenusController < ApplicationController
   end
 
   def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      flash[:success] = "メニューを投稿しました！"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
+
+  private
+    def menu_params
+      params.require(:menu).permit(:name, :price, :explain, :image).merge(user_id: current_user.id)
+    end
 end
