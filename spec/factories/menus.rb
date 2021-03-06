1e7100ee3b536538d737c0c8ccc1c419225f8f79
takaya-47 <local.s.s.47@gmail.com>
2021-03-06 18:22:47 +0900

    FactoryBot作成とメニュー投稿が成功するテスト

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :menu do
-    name { "MyString" }
-    price { 1 }
-    explain { "MyText" }
-    user { nil }
+    name { "オムライス" }
+    price { 1000 }
+    explain { "懐かしのオムライスです" }
+    association :user
+
+    after(:build) do |menu|
+      menu.image.attach(io: File.open('app/assets/images/test.jpeg'), filename: 'test.jpeg')
+    end
   end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @menu = FactoryBot.build(:menu)
+  end
+
+  describe "メニューの新規投稿" do
+    context "メニューの投稿ができるとき" do
+      it "すべての情報が正しく入力されていれば投稿できる" do
+        expect(@menu).to be_valid
+      end
+    end
+
+    context "メニューの投稿ができないとき" do
+
+    end
+  end
 end
