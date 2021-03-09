6a98956814f34f75e2de58699bacacf2ebc780d6
takaya-47 <local.s.s.47@gmail.com>
2021-03-09 12:23:35 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,12 +68,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # herokuのMailgunを使用するための設定
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://take-out-app.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'https://take-out-app.herokuapp.com/',
+  :domain         => host,
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
