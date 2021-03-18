1cacce04450f159896fba124484c1ad277edf0f9
takaya-47 <local.s.s.47@gmail.com>
2021-03-18 15:36:28 +0900

    RuboCopの実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -48,7 +48,7 @@ class OrdersController < ApplicationController
   def move_to_root
     find_menu
     if current_user.id == @menu.user.id
-      flash[:alert] = "権限がありません"
+      flash[:alert] = '権限がありません'
       redirect_to root_path
     end
   end
