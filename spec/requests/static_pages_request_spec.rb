4f550cd32ed42d404fa581e0509826465832db62
takaya-47 <local.s.s.47@gmail.com>
2021-03-25 16:18:06 +0900

    RSpecの修正

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -5,9 +5,4 @@ RSpec.describe 'StaticPages', type: :request do
     get root_path
     expect(response.status).to eq 200
   end
-
-  it 'contactアクションにリクエストすると正常にレスポンスが返ってくる' do
-    get static_pages_contact_path
-    expect(response.status).to eq 200
-  end
 end
