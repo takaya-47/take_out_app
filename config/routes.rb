2911d11fca390073ceb7f2f7b6296002b0728ed3
takaya-47 <local.s.s.47@gmail.com>
2021-03-25 16:07:07 +0900

    Contactページの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/contact', to: "static_pages#contact"
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show, :index]
